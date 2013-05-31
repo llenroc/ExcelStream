8ee9e4f8c7ea29b6660beafddc27438332d940be
Oliver, Jonathan <jonathan@localhost>
2013-05-31 14:03:19 -0600

    Initial commit.

---
+++ src/ExcelStream/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExcelStream")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("30b1a822-66b6-49e4-ab2b-6f96a8c04a8d")]
---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------
+// <copyright file="GlobalAssemblyInfo.cs" company="SmartyStreets, LLC">
+//     Copyright (c) SmartyStreets, LLC. All rights reserved.
+// </copyright>
+// <author>Oliver, Jonathan</author>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("SmartyStreets, LLC")]
+[assembly: AssemblyProduct("ExcelStream")]
+[assembly: AssemblyCopyright("Copyright © SmartyStreets, LLC 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
