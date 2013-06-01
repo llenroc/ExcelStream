ac72ca7b442e7c1c966f9957e87194d66243c88a
Oliver, Jonathan <jonathan@localhost>
2013-05-31 22:34:01 -0600

    Added NuGet.

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0 Release Build 0 (2013-06-03)")]
+[assembly: AssemblyInformationalVersion("1.0 Release Build 0 (2013-06-01)")]
