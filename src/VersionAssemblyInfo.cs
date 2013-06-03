4ce5ba95d618804e90d22efedda0d231b606051d
Oliver, Jonathan <jonathan@localhost>
2013-06-03 13:56:56 -0600

    Bumped version.

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 //------------------------------------------------------------------------------
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
 [assembly: AssemblyInformationalVersion("1.0 Release Build 3 (2013-06-01)")]
