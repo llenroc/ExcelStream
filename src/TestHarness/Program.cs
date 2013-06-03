7d56296982f9a5c10d77844c011419683d4e9bef
Oliver, Jonathan <jonathan@localhost>
2013-06-03 12:24:50 -0600

    Added using pattern to test harness.

--- src/TestHarness/Program.cs
+++ src/TestHarness/Program.cs
@@ -1,26 +1,28 @@
 ﻿namespace TestHarness
 {
 	using System;
+	using System.Globalization;
 	using ExcelStream;
 
 	class Program
 	{
 		public static void Main()
 		{
-			var writer = new ExcelWriter("c:/" + DateTime.UtcNow.Ticks + ".xlsx");
-			var cells = new string[11];
-
-			writer.AppendRow(new string[] { "<Hello>", "\"'&World!" });
-			for (var i = 0; i < 101; i++)
+			using (var writer = new ExcelWriter("c:/" + DateTime.UtcNow.Ticks + ".xlsx"))
 			{
-				for (var x = 0; x < cells.Length; x++)
-					cells[x] = (i * x).ToString();
+				var cells = new string[11];
 
-				writer.AppendRow(cells);
+				writer.AppendRow(new[] { "Hello", "World!" });
+				for (var i = 0; i < 101; i++)
+				{
+					for (var x = 0; x < cells.Length; x++)
+						cells[x] = (i * x).ToString(CultureInfo.InvariantCulture);
+
+					writer.AppendRow(cells);
+				}
+
+				writer.Save();
 			}
-				
-			writer.Save();
-			writer.Dispose();
 		}
 	}
 }
